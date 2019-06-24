ca5d629bce2968ed67620e20a95e5a04330448ab
krystian <m.krystian@op.pl>
2019-06-24 23:55:07 +0200

    adding default exception type

--- FileReadWriteException.cpp
+++ FileReadWriteException.cpp
@@ -2,26 +2,29 @@
 
 FileReadWriteException::FileReadWriteException(int type) : type(type)
 {
-	switch (type)
-	{
-	case 0:
-		exeptionType = "Invalid Characters Type in file! Only Allowed : \"0\", \"1\", \"N\", \"O\", \"H\" !";
-		break;
-	case 1:
-		exeptionType = "Invalid Characters Sequence in file!\nAfter: character there should be a space and then \"0\" or \"1\" !";
-		break;
-	case 2:
-		exeptionType = "Could not open the file to read!";
-		break;
-	case 3:
-		exeptionType = "Could not open the file to write!";
-		break;
-	}
+    switch (type)
+    {
+        case 0:
+            exeptionType = "Invalid Characters Type in file! Only Allowed : \"0\", \"1\", \"N\", \"O\", \"H\" !";
+            break;
+        case 1:
+            exeptionType = "Invalid Characters Sequence in file!\nAfter: character there should be a space and then \"0\" or \"1\" !";
+            break;
+        case 2:
+            exeptionType = "Could not open the file to read!";
+            break;
+        case 3:
+            exeptionType = "Could not open the file to write!";
+            break;
+        default:
+            exeptionType = "Default exception";
+            break;
+    }
 }
 
-const char * FileReadWriteException::what() const throw()
+const char* FileReadWriteException::what() const throw()
 {
-	return exeptionType;
+    return exeptionType;
 }
 
 
